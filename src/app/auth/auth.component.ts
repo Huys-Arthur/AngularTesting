ede7f10f15d735ea1e3b1ec58f88d214d2dce762
Arthur Huys <arthur.huys@student.vives.be>
2021-11-10 17:44:27 +0100

    Exchange the Code For a Token, attempt 1

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 
 @Component({
@@ -10,13 +11,28 @@ import { ActivatedRoute } from '@angular/router';
 export class AuthComponent implements OnInit {
   public code: string = "";
   
-  constructor(private activatedRoute: ActivatedRoute) {
-    this.activatedRoute.queryParams.subscribe(params => {
-          this.code = params['code'];
-          console.log(this.code);
-      });
+  constructor(private activatedRoute: ActivatedRoute, private http: HttpClient) {
   }
 
   ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.code = params['code'];
+      console.log(this.code);
+      this.sendPostRequest();
+    });
   }
+
+  sendPostRequest() {
+    let headers = { 
+      'client-id': '424887959161689',
+      'client_secret': '9a74a81abf60f654a31d66461c0d94da',
+      'grant_type': 'authorization_code',
+      'redirect_uri': 'https://huys-arthur.github.io/AngularTesting/auth/', 
+      'code': `${this.code}`};
+    console.log(headers);
+    const body = { title: 'Angular POST Request' };
+    this.http.post<any>('https://api.instagram.com/oauth/access_token', body, { headers }).subscribe(data => {
+      console.log(data);
+  });;
+} 
 }
