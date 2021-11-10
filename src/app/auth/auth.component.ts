829a587a273b19577862147a2be9338a344b9484
Arthur Huys <arthur.huys@student.vives.be>
2021-11-10 16:22:13 +0100

    added code to get AuthCode from url

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-auth',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
-
-  constructor() { }
+  public code: string = "";
+  
+  constructor(private activatedRoute: ActivatedRoute) {
+    this.activatedRoute.queryParams.subscribe(params => {
+          this.code = params['code'];
+          console.log(this.code);
+      });
+  }
 
   ngOnInit(): void {
   }
-
 }
