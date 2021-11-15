2e6e964cc296d5a3473e3982b82029651bfc00ce
Arthur Huys <arthur.huys@student.vives.be>
2021-11-15 10:20:13 +0100

    removed account_type from GET

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -46,10 +46,9 @@ export class AuthComponent implements OnInit {
   }
 
   GenerateUserData(user_id:string, access_token:string){
-    this.http.get<any>("https://graph.instagram.com/v12.0/" + this.user_id + "?fields=account_type,id,username,&access_token=" + this.access_token).subscribe(data => {
+    this.http.get<any>("https://graph.instagram.com/v12.0/" + this.user_id + "?fields=id,username,&access_token=" + this.access_token).subscribe(data => {
       console.log(data);
       if(data != null){
-        this.account_type = data["account_type"];
         this.id = data["id"];
         this.username = data["username"];
       }
